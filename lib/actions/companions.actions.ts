e4018c1864685de91b5ee48f3dfccf3ed9a8a79a
TaherMustansir1929 <eren.jeager1929101@gmail.com>
2025-06-03 18:53:18 +0500

    Implement Profile-Page

--- lib/actions/companions.actions.ts
+++ lib/actions/companions.actions.ts
@@ -82,7 +82,7 @@ export const getRecentSessions = async (limit = 10) => {
     .order("created_at", { ascending: false })
     .limit(limit);
 
-  if(error) throw new Error(error.message);
+  if (error) throw new Error(error.message);
 
   return data.map(({ companions }) => companions);
 };
@@ -96,7 +96,19 @@ export const getUserSessions = async (userId: string, limit = 10) => {
     .order("created_at", { ascending: false })
     .limit(limit);
 
-  if(error) throw new Error(error.message);
+  if (error) throw new Error(error.message);
 
   return data.map(({ companions }) => companions);
+};
+
+export const getUserCompanions = async (userId: string) => {
+  const supabase = createSupabaseClient();
+  const { data, error } = await supabase
+    .from("companions")
+    .select()
+    .eq("author", userId);
+
+  if (error) throw new Error(error.message);
+
+  return data;
 };
